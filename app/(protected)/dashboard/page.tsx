ecba81dbb4df1e921c8bf1f0bca69274b1abeecf
leroy-powerup <leroy.michael@powerup.id>
2024-03-01 12:34:59 +0700

    remove debugger

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -77,7 +77,6 @@ export default function DashboardPage() {
   }, [session]);
   return (
     <>
-      {selectMonth}
       <div className="flex-col md:flex">
         <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
